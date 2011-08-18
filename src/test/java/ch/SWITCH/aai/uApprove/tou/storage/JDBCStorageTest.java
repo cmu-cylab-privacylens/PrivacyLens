c5744798582d190cb1b0243551429b13e8d4f3bf
reusser <reusser@630430ee-30e2-4e44-a323-da843f692fab>
2011-08-18 14:26:51 +0000

    JDBC tests enabled
    git-svn-id: https://subversion.switch.ch/svn/general/aai/uApprove/trunk@2051 630430ee-30e2-4e44-a323-da843f692fab

---
+++ src/test/java/ch/SWITCH/aai/uApprove/tou/storage/JDBCStorageTest.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.SWITCH.aai.uApprove.tou.storage;
+
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import ch.SWITCH.aai.uApprove.tou.ToUAcceptance;
+
+/**
+ * Tests JDBC storage using the Spring JDBC framework.
+ */
+
+@ContextConfiguration(locations = {"/uApprove-test.xml"})
+@TransactionConfiguration(defaultRollback = true)
+public class JDBCStorageTest extends AbstractTransactionalTestNGSpringContextTests {
+
+    private final Logger logger = LoggerFactory.getLogger(JDBCStorageTest.class);
+
+    @Autowired
+    private Storage storage;
+
+    @BeforeClass
+    @Rollback(false)
+    public void initialize() {
+        super.executeSqlScript("classpath:/tou-schema.sql", false);
+    }
+
+    @Test
+    public void crudToUAcceptance() {
+
+        final String userId = "student1";
+        final String version = "1.0";
+        final String fingerprint = "5b2ee897c08c79a09cd57e8602d605bf8c52db17de9793677c36b5c78644b2b2";
+        final DateTime acceptanceDate = new DateTime("2011-11-11T11:11:11");
+
+        AssertJUnit.assertFalse(storage.containsToUAcceptance(userId, version));
+        AssertJUnit.assertNull(storage.readToUAcceptance(userId, version));
+
+        ToUAcceptance touAcceptance = new ToUAcceptance(version, fingerprint, acceptanceDate);
+        storage.createToUAcceptance(userId, touAcceptance);
+        AssertJUnit.assertTrue(storage.containsToUAcceptance(userId, version));
+
+        touAcceptance = storage.readToUAcceptance(userId, version);
+
+        AssertJUnit.assertEquals(version, touAcceptance.getVersion());
+        AssertJUnit.assertEquals(fingerprint, touAcceptance.getFingerprint());
+        AssertJUnit.assertEquals(acceptanceDate, touAcceptance.getAcceptanceDate());
+
+        touAcceptance = new ToUAcceptance(version, fingerprint.substring(1), acceptanceDate.plusMonths(1));
+        storage.updateToUAcceptance(userId, touAcceptance);
+
+        touAcceptance = storage.readToUAcceptance(userId, version);
+        AssertJUnit.assertEquals(version, touAcceptance.getVersion());
+        AssertJUnit.assertEquals(fingerprint.substring(1), touAcceptance.getFingerprint());
+        AssertJUnit.assertEquals(acceptanceDate.plusMonths(1), touAcceptance.getAcceptanceDate());
+    }
+}
