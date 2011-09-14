5be3c107974e750e64fc02f20a2877c53367ee84
reusser <reusser@630430ee-30e2-4e44-a323-da843f692fab>
2011-09-14 15:43:01 +0000

    Storage Migration Script
    git-svn-id: https://subversion.switch.ch/svn/general/aai/uApprove/trunk@2107 630430ee-30e2-4e44-a323-da843f692fab

--- src/main/groovy/StorageMigration.groovy
+++ src/main/groovy/StorageMigration.groovy
@@ -15,12 +15,66 @@
  * limitations under the License.
  */
 
-/**
- *
- */
-class StorageMigration {
+import groovy.sql.Sql
 
-    static void main(def args){
-        println "Groovy!"
+assert this.args.size() == 2
+
+def properties = new Properties()
+new File(this.args[0]).withInputStream { stream ->
+    properties.load(stream)
+}
+
+new File(this.args[1]).withInputStream { stream ->
+    properties.load(stream)
+}
+
+def fromSql = Sql.newInstance(properties['url'], properties['user'], properties['password'], properties['driver'])
+def toSql = Sql.newInstance(properties['database.url'], properties['database.user'], properties['database.password'], properties['database.driver'])
+
+println "Terms Of Use Migration"
+
+def touAcceptance = toSql.dataSet("ToUAcceptance");
+fromSql.eachRow('SELECT auUserName, auLastTermsVersion, auFirstAccess FROM ArpUser') {
+    def userId = it['auUserName']
+    def version = it['auLastTermsVersion']
+    def fingerprint = 'n/a'
+    def acceptanceDate = it['auFirstAccess']
+    println "Migrating ToU Acceptance of ${userId} for version ${version}."
+    try {
+        touAcceptance.add(userId:userId, version:version, fingerprint:fingerprint, acceptanceDate:acceptanceDate)
+    } catch (SQLIntegrityConstraintViolationException) {
+        // Ignore, but continue
     }
 }
+
+
+println "Attribute Release Consent Migration"
+def attributeReleaseConsent = toSql.dataSet("AttributeReleaseConsent");
+fromSql.eachRow('SELECT au.auUserName, sp.spProviderName, ara.araAttributes, ara.araTimeStamp FROM ArpUser au, ShibProvider sp, AttrReleaseApproval ara WHERE ara.araIdxArpUser = au.IdxArpUser AND ara.araIdxShibProvider = sp.IdxShibProvider') {
+
+    def userId = it['auUserName']
+    def relyingPartyId = it['spProviderName']
+    def attributes = it['araAttributes']
+    def consentDate = it['araTimeStamp']
+
+    if (!relyingPartyId) {
+        println "Migrating general Attribute Release Consent of ${userId}."
+        try {
+            attributeReleaseConsent.add(userId:userId, relyingPartyId:'*', attributeId:'*', valuesHash:"n/a", consentDate: consentDate);
+        }catch (SQLIntegrityConstraintViolationException) {
+            // Ignore, but continue
+        }
+    } else {
+        attributes.split(":").each {
+            if (it) {
+                println "Migrating Attribute Release Consent of ${userId} to ${relyingPartyId} for ${it}."
+                try {
+                    attributeReleaseConsent.add(userId:userId, relyingPartyId:relyingPartyId, attributeId:it, valuesHash:"n/a", consentDate: consentDate);
+                }catch (SQLIntegrityConstraintViolationException) {
+                    // Ignore, but continue
+                }
+            }
+        }
+    }
+}
+
